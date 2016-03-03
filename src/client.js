60eeba33caf9e66e5e49e460b904b20ca4f76005
Mikko Forsström <puhemies@diktaattoriporssi.com>
2016-03-03 10:30:19 +0200

    Removed dependencies that come from broilerplate.

--- src/client.js
+++ src/client.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { browserHistory as history } from 'react-router';
+import { browserHistory } from 'react-router';
 import { createStore } from '@dr-kobros/react-broilerplate/lib/redux';
 import { createApp } from '@dr-kobros/react-broilerplate/lib/app';
 import * as reducers from './reducers';
 import { createRouter } from './router';
 
-const store = createStore(reducers, history);
+const { store, history } = createStore(reducers, browserHistory);
 
 const router = createRouter({
     store,
